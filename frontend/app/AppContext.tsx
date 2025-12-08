c660d734ac806e7dd0564c667fd8e6c80a579c13
Ammar Riaz <futuristicgamer2321@gmail.com>
2025-12-09 03:01:58 +0500

    comine

--- frontend/app/AppContext.tsx
+++ frontend/app/AppContext.tsx
@@ -1,7 +1,1 @@
-import { createContext, useContext } from "react";
 
-export const AppContext = createContext({
-  openChatFromTab: (_chatId: number, _tripId: number) => {},
-});
-
-export const useRootNavigation = () => useContext(AppContext);
