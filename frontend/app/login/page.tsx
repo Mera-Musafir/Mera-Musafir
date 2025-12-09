add4c6d3bcb54c1164661adfe3e358035797b362
hamzabread <hamzaval2000@gmail.com>
2025-12-09 17:06:46 +0500

    new-frontend

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../components/Login'
+import Navbar from '../components/Navbar'
+
+const page = () => {
+  return (
+    <>
+        <Navbar />
+        <Login />
+    </>
+  )
+}
+
+export default page
